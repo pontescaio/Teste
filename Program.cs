fc2af047b81530b7e10aeca09d674550a7538ab6
Caio Pontes <caiopontesgs15@gmail.com>
2021-11-08 22:11:04 -0300

    Projeto de teste

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TopicosEspeciais {
+    class Program {
+        static void Main(string[] args) {
+
+            TimeSpan t1 = new TimeSpan(1, 30, 0);
+
+            TimeSpan mult = t1.Multiply(2.0);
+
+            Console.WriteLine(mult);
+
+        }
+    }
+}
